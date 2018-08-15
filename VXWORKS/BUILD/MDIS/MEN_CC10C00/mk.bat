0fd4785476c839145263190e936176f0ec38ca2b
Thomas Schnuerer <Thomas.Schnuerer@men.de>
2018-08-15 14:27:49 +0200

    R: 1. MDIS project for 15CC10C00 did not work correctly with MDIS wizard generated Makefile and system.dsc    2. SW modules were reported to be missing in MDIS Wizard upon CC10C00 project generation M: 1. Manually corrected PCI location and used FPGA BBIS driver (chameleon instead chameleon_pcitbl)    2. Added SMB2_API and SMB2_SHC submodules (not needed if related smb2 utilities not selected)

--- VXWORKS/BUILD/MDIS/MEN_CC10C00/mk.bat
+++ VXWORKS/BUILD/MDIS/MEN_CC10C00/mk.bat
@@ -1,7 +1,7 @@
-@echo Internal test tool for MEN
+@echo Internal test wrapper for MEN to build object and copy to used VIP folder at once - ADAPT TO OWN FOLDER!
 cls
 make clean
 make
-@REM copy the MDIS object to the BSP work folder for linkage 
+@REM copy the MDIS object to the BSP work folder for linkage
 @REM object with debugs
-copy /Y C:\work\work_VXdeploy\VXWORKS\LIB\MEN\vxworks-6.9_mdis_MEN_CC10C\objARMARCH7gnutest\mdis_MEN_CC10C.o C:\work\work_10CC10-60\vxworks-6.9_mdis_MEN_CC10C\objARMARCH7gnutest\mdis_MEN_CC10C.o
+copy /Y c:\work\work_VXdeploy\VXWORKS\LIB\MEN\vxworks-6.9_mdis_MEN_CC10C00\objARMARCH7gnutest\mdis_MEN_CC10C00.o c:\work\10CC10-60\vxworks-6.9_mdis_MEN_CC10C00\objARMARCH7gnutest\mdis_MEN_CC10C00.o
